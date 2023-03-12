58a390d8fe521c851a2ca86462b70c4466aa442b
edeotena <edeotena13@gmail.com>
2023-03-12 22:23:50 +0300

    Sub for different sign values

--- src/lib/arithmetics.c
+++ src/lib/arithmetics.c
@@ -2,7 +2,7 @@
 
 int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
   null_decimal(result);
-  unsigned int scale = get_exponent(&value_1);
+  unsigned int scale;
   int ret = scale_decimals(&value_1, &value_2, &scale);
   set_exponent(result, scale);
   if (get_sign(&value_1) == POSITIVE && get_sign(&value_2) == POSITIVE) {
@@ -16,7 +16,18 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
 }
 
 int s21_sub(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
-  return OK;
+  null_decimal(result);
+  unsigned int scale;
+  int ret = scale_decimals(&value_1, &value_2, &scale);
+  set_exponent(result, scale);
+  if (get_sign(&value_1) == POSITIVE && get_sign(&value_2) == NEGATIVE) {
+    ret = add_one_sign_decimals(&value_1, &value_2, result);
+    set_sign(result, POSITIVE);
+  } else if (get_sign(&value_1) == NEGATIVE && get_sign(&value_2) == POSITIVE) {
+    ret = add_one_sign_decimals(&value_1, &value_2, result);
+    set_sign(result, NEGATIVE);
+  }
+  return ret;
 }
 
 int s21_mul(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
