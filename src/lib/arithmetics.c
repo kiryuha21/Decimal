c669b857e1ceec547c448ab66e41a177369567a2
lyradanu <lyradanu@student.21-school.ru>
2023-03-16 18:08:38 +0300

    s21_add modified, all comparison functions done

--- src/lib/arithmetics.c
+++ src/lib/arithmetics.c
@@ -5,18 +5,21 @@ int s21_add(s21_decimal value_1, s21_decimal value_2, s21_decimal *result) {
   unsigned int scale;
   int ret = scale_decimals(&value_1, &value_2, &scale);
   set_exponent(result, scale);
-  if (get_sign(&value_1) == POSITIVE && get_sign(&value_2) == POSITIVE) {
+
+  int first_sign = get_sign(&value_1);
+  if (get_sign(&value_1) == get_sign(&value_2)) {
     ret = add_one_sign_decimals(&value_1, &value_2, result);
-    set_sign(result, POSITIVE);
-  } else if (get_sign(&value_1) == NEGATIVE && get_sign(&value_2) == NEGATIVE) {
-    ret = add_one_sign_decimals(&value_1, &value_2, result);
-    set_sign(result, NEGATIVE);
-  } else if (get_sign(&value_1) == NEGATIVE && get_sign(&value_2) == POSITIVE) {
-    ret = sub_diff_sign_decimals(&value_1, &value_2,
-                                 result);  // TODO: handle res sign in sub
-  } else if (get_sign(&value_1) == POSITIVE && get_sign(&value_2) == NEGATIVE) {
-    ret = sub_diff_sign_decimals(&value_1, &value_2,
-                                 result);  // TODO: handle res sign in sub
+    if (first_sign == POSITIVE) {
+      set_sign(result, POSITIVE);
+    } else {
+      set_sign(result, NEGATIVE);
+    }
+  } else {
+    ret = sub_diff_sign_decimals(&value_1, &value_2, result);
+    if (first_sign == NEGATIVE) {
+      int res_sign = get_sign(result);
+      set_sign(result, res_sign == POSITIVE ? NEGATIVE : POSITIVE);
+    }
   }
   return ret;
 }
--- src/lib/comparisons.c
+++ src/lib/comparisons.c
@@ -1,30 +1,84 @@
 #include "../s21_decimal.h"
 
-int s21_is_less(s21_decimal first, s21_decimal second) { return TRUE; }
+int s21_is_less(s21_decimal first, s21_decimal second) {
+    unsigned int scale;
+    if (scale_decimals(&first, &second, &scale) != OK) {
+        return FALSE;
+    }
 
-int s21_is_less_or_equal(s21_decimal first, s21_decimal second) { return TRUE; }
+    int first_sign = get_sign(&first);
+    int second_sign = get_sign(&second);
 
-int s21_is_greater(s21_decimal first, s21_decimal second) { return TRUE; }
+    if (!is_zero(&first) && !is_zero(&second)) {
+        if (first_sign == POSITIVE && second_sign == NEGATIVE) {
+            return FALSE;
+        } else if (first_sign == NEGATIVE && second_sign == POSITIVE) {
+            return TRUE;
+        }
+    }
+
+    for (int i = 0; i < 3; ++i) {
+        if (first.bits[i] < second.bits[i]) {
+            return first_sign == POSITIVE ? TRUE : FALSE;
+        }
+    }
+
+    return FALSE;
+}
+
+int s21_is_less_or_equal(s21_decimal first, s21_decimal second) {
+  return s21_is_greater(first, second) == TRUE ? FALSE : TRUE;
+}
+
+int s21_is_greater(s21_decimal first, s21_decimal second) {
+  unsigned int scale;
+  if (scale_decimals(&first, &second, &scale) != OK) {
+    return FALSE;
+  }
+
+  int first_sign = get_sign(&first);
+  int second_sign = get_sign(&second);
+
+  if (!is_zero(&first) && !is_zero(&second)) {
+    if (first_sign == POSITIVE && second_sign == NEGATIVE) {
+      return TRUE;
+    } else if (first_sign == NEGATIVE && second_sign == POSITIVE) {
+      return FALSE;
+    }
+  }
+
+  for (int i = 0; i < 3; ++i) {
+    if (first.bits[i] > second.bits[i]) {
+      return first_sign == POSITIVE ? TRUE : FALSE;
+    }
+  }
+
+  return FALSE;
+}
 
 int s21_is_greater_or_equal(s21_decimal first, s21_decimal second) {
-  return TRUE;
+  return s21_is_less(first, second) == TRUE ? FALSE : TRUE;
 }
 
 int s21_is_equal(s21_decimal first, s21_decimal second) {
-  int ret = TRUE;
   unsigned int scale;
   if (scale_decimals(&first, &second, &scale) != OK) {
-    ret = FALSE;
+    return FALSE;
   }
-  for (int i = 0; i < 2; ++i) {
+
+  for (int i = 0; i < 3; ++i) {
     if (first.bits[i] != second.bits[i]) {
-      ret = FALSE;
+      return FALSE;
     }
   }
+
   if (!is_zero(&first) && first.bits[3] != second.bits[3]) {
-    ret = FALSE;
+    return FALSE;
   }
-  return ret;
+
+  return TRUE;
 }
 
-int s21_is_not_equal(s21_decimal first, s21_decimal second) { return TRUE; }
+int s21_is_not_equal(s21_decimal first, s21_decimal second) {
+  return s21_is_equal(first, second) == TRUE ? FALSE : TRUE;
+}
