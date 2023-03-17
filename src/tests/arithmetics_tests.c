3aa703bcab9d301b1bc7c4584b35942654599f41
savagepa <savagepa@21-school.ru>
2023-03-17 16:11:12 +0300

    Some tests for add

--- src/tests/arithmetics_tests.c
+++ src/tests/arithmetics_tests.c
@@ -1,3 +1,128 @@
+#include <stdio.h>
+
 #include "s21_decimal_testing.h"
 
-Suite* get_arithmetics_suite() {}
+// -----------add incorrect inp-----------
+START_TEST(add_null_inp) {
+  s21_decimal a = DEFAULT_DECIMAL, b = DEFAULT_DECIMAL;
+
+  ck_assert_int_eq(s21_add(a, b, NULL), ERROR);
+}
+END_TEST
+
+START_TEST(add_too_lardge_inp) {
+  s21_decimal a = MAX_DECIMAL, b = MAX_DECIMAL, c = DEFAULT_DECIMAL;
+
+  ck_assert_int_eq(s21_add(a, b, &c), ERROR);
+}
+END_TEST
+
+START_TEST(add_too_small_inp) {
+  s21_decimal a = MIN_DECIMAL, b = MIN_DECIMAL, c = DEFAULT_DECIMAL;
+
+  ck_assert_int_eq(s21_add(a, b, &c), ERROR);
+}
+END_TEST
+// -----------add correct inp-------------
+START_TEST(add_positive_numb) {
+  s21_decimal a = {5, 0, 0, 0}, b = {5, 0, 0, 0}, c = DEFAULT_DECIMAL;
+
+  ck_assert_int_eq(s21_add(a, b, &c), OK);
+  ck_assert_uint_eq(c.bits[0], 10);
+}
+END_TEST
+
+START_TEST(add_positive_greater_negative_numb) {
+  s21_decimal a = {7, 0, 0, 0}, b = {5, 0, 0, 1 << 31}, c = DEFAULT_DECIMAL;
+
+  ck_assert_int_eq(s21_add(a, b, &c), OK);
+  ck_assert_uint_eq(c.bits[0], 2);
+  ck_assert_uint_eq(c.bits[3], 0);
+}
+END_TEST
+
+START_TEST(add_positive_less_negative_numb) {
+  s21_decimal a = {5, 0, 0, 0}, b = {7, 0, 0, 1 << 31}, c = DEFAULT_DECIMAL;
+
+  ck_assert_int_eq(s21_add(a, b, &c), OK);
+  ck_assert_uint_eq(c.bits[0], 2);
+  ck_assert_uint_eq(c.bits[3], 1 << 31);
+}
+END_TEST
+
+START_TEST(add_negative_numb) {
+  s21_decimal a = {7, 0, 0, 1 << 31}, b = {5, 0, 0, 1 << 31},
+              c = DEFAULT_DECIMAL;
+
+  ck_assert_int_eq(s21_add(a, b, &c), OK);
+  ck_assert_uint_eq(c.bits[0], 12);
+}
+END_TEST
+
+START_TEST(add_shift_0_to_1_bits) {
+  s21_decimal a = {1 << 31, 0, 0, 0}, b = {1 << 31, 0, 0, 0},
+              c = DEFAULT_DECIMAL;
+
+  ck_assert_int_eq(s21_add(a, b, &c), OK);
+  ck_assert_uint_eq(c.bits[1], 1);
+  ck_assert_uint_eq(c.bits[0], 0);
+}
+END_TEST
+
+START_TEST(add_shift_1_to_0_bits) {
+  s21_decimal a = {0, 1, 0, 0}, b = {1 << 31, 0, 0, 1 << 31},
+              c = DEFAULT_DECIMAL;
+
+  ck_assert_int_eq(s21_add(a, b, &c), OK);
+  ck_assert_uint_eq(c.bits[0], 1 << 31);
+  ck_assert_uint_eq(c.bits[1], 0);
+  ck_assert_uint_eq(c.bits[3], 0);
+}
+END_TEST
+
+START_TEST(add_shift_1_to_2_bits) {
+  s21_decimal a = {0, 1 << 31, 0, 0}, b = {0, 1 << 31, 0, 0},
+              c = DEFAULT_DECIMAL;
+
+  ck_assert_int_eq(s21_add(a, b, &c), OK);
+  ck_assert_uint_eq(c.bits[2], 1);
+  ck_assert_uint_eq(c.bits[1], 0);
+}
+END_TEST
+
+START_TEST(add_shift_2_to_1_bits) {
+  s21_decimal a = {0, 0, 1, 0}, b = {0, 1 << 31, 0, 1 << 31},
+              c = DEFAULT_DECIMAL;
+
+  ck_assert_int_eq(s21_add(a, b, &c), OK);
+  ck_assert_uint_eq(c.bits[1], 1 << 31);
+  ck_assert_uint_eq(c.bits[2], 0);
+  ck_assert_uint_eq(c.bits[3], 0);
+}
+END_TEST
+
+Suite *get_arithmetics_suite() {
+  Suite *s;
+  TCase *t_case;
+
+  s = suite_create("Arithmetics suite");
+
+  t_case = tcase_create("Add incorrect inp");
+  tcase_add_test(t_case, add_null_inp);
+  tcase_add_test(t_case, add_too_lardge_inp);
+  tcase_add_test(t_case, add_too_small_inp);
+  suite_add_tcase(s, t_case);
+
+  t_case = tcase_create("Add correct inp");
+  tcase_add_test(t_case, add_positive_numb);
+  tcase_add_test(t_case, add_positive_greater_negative_numb);
+  tcase_add_test(t_case, add_positive_less_negative_numb);
+  tcase_add_test(t_case, add_negative_numb);
+  tcase_add_test(t_case, add_shift_0_to_1_bits);
+  tcase_add_test(t_case, add_shift_1_to_0_bits);
+  tcase_add_test(t_case, add_shift_1_to_2_bits);
+  tcase_add_test(t_case, add_shift_2_to_1_bits);
+  suite_add_tcase(s, t_case);
+
+  return s;
+}
