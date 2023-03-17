41e6cb0e6ca0ecdd006da15b39889ce45fccbb0e
kiryuha21 <kiryuha.randomnumbers@gmail.com>
2023-03-17 11:59:04 +0300

    all conversions made, though some errors may be unhandled

--- src/lib/conversions.c
+++ src/lib/conversions.c
@@ -1,9 +1,83 @@
 #include "../s21_decimal.h"
 
-int s21_from_int_to_decimal(int src, s21_decimal *dst) { return OK; }
+#include <limits.h>
+#include <math.h>
 
-int s21_from_float_to_decimal(float src, s21_decimal *dst) { return OK; }
+int s21_from_int_to_decimal(int src, s21_decimal *dst) {
+    null_decimal(dst);
 
-int s21_from_decimal_to_int(s21_decimal src, int *dst) { return OK; }
+    if (src < 0) {
+        set_sign(dst, NEGATIVE);
+        src = -src;
+    }
+    dst->bits[2] = src;
 
-int s21_from_decimal_to_float(s21_decimal src, float *dst) { return OK; }
+    return OK;
+}
+
+int s21_from_float_to_decimal(float src, s21_decimal *dst) {
+    if (src < 1e-28) {
+        null_decimal(dst);
+        return ERROR;
+    }
+
+    if (src == INFINITY || src == NAN) {
+        return ERROR;
+    }
+
+    null_decimal(dst);
+    if (src < 0) {
+        set_sign(dst, NEGATIVE);
+        src = -src;
+    }
+
+    int integer_part = (int)src;
+    int significant_digits = 0;
+    for (; integer_part > 0; integer_part /= 10, ++significant_digits);
+
+    int exp = 7 - significant_digits;
+    set_exponent(dst, exp);
+
+    dst->bits[2] = (unsigned int)powf(10, (float)exp);
+
+    return OK;
+}
+
+int s21_from_decimal_to_int(s21_decimal src, int *dst) {
+    s21_decimal integer_src;
+    s21_truncate(src, &integer_src);
+    reduce_exponent(&src);
+
+    if (get_elder_bit_index(&integer_src) != 2) {
+        return ERROR;
+    }
+
+    if (integer_src.bits[2] > INT_MAX) {
+        return ERROR;
+    }
+
+    *dst = (int)integer_src.bits[2];
+    if (get_sign(&integer_src) == NEGATIVE) {
+        *dst = -(*dst);
+    }
+
+    return OK;
+}
+
+int s21_from_decimal_to_float(s21_decimal src, float *dst) {
+    reduce_exponent(&src);
+
+    int int_part;
+    float float_part;
+
+    if (s21_from_decimal_to_int(src, &int_part) != OK) {
+        return ERROR;
+    }
+
+    unsigned int exp = get_exponent(&src);
+    float power = powf(10, (float)exp);
+    float_part = (float)(src.bits[2] % (int)power) * powf(0.1F, power);
+
+    *dst = (float)int_part + float_part;
+    return OK;
+}
