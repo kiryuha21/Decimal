d44e57ffe273704867e90e8fb0df1f66452cbd7b
Guini Cybernetic <guinicyb@mi-q6.msk.21-school.ru>
2023-03-18 19:20:05 +0300

    2 Test fixed

--- src/main.c
+++ src/main.c
@@ -3,9 +3,12 @@
 #include "s21_decimal.h"
 
 int main() {
-  s21_decimal a = {{1, 1, 1, -2147418112}}, b = {0, 0xFFFFFFFF, 0, 0},
-              c = DEFAULT_DECIMAL;
-  int ret = s21_sub(a, a, &c);
-  print_decimal(&c);
-  printf("%i", ret);
+  s21_decimal x = {{1, 0, 0, 655360}};
+  s21_decimal y = {{1, 0, 0, 655360}};
+  s21_decimal z = {{0, 0, 0, 0}};
+  s21_mul(x, y, &z);
+  char ourRes[1000], expect[1000] = "1 0 0 1310720";
+  snprintf(ourRes, sizeof(char) * 1000, "%u %u %u %u", z.bits[0], z.bits[1],
+           z.bits[2], z.bits[3]);
+  printf("%s", ourRes);
 }
