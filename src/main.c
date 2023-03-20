8bfd9077ea78d2ddc0c55166410456b02e66b679
edeotena <edeotena13@gmail.com>
2023-03-21 01:09:04 +0300

    Some more tests (BUT looks like bad testsgit add -Agit add -Agit add -Agit add -Agit add -Agit add -A(at least 1 that i looked for))

--- src/main.c
+++ src/main.c
@@ -3,15 +3,8 @@
 #include "s21_decimal.h"
 
 int main() {
-  s21_decimal a = {0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0},
-              b = {4, 0, 0, 0x00010000}, c = DEFAULT_DECIMAL;
-
-  s21_decimal x = {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0}};
-  s21_decimal y = {{6, 0, 0, 65536}};
-  s21_decimal z = {{0, 0, 0, 0}};
-  int ret = s21_add(x, y, &z);
-  char ourRes[1000], expect[1000] = "0XFFFFFFFF 0XFFFFFFFF 0XFFFFFFFF 00000000";
-  snprintf(ourRes, sizeof(char) * 1000, "%#.8X %#.8X %#.8X %#.8X", z.bits[0],
-           z.bits[1], z.bits[2], z.bits[3]);
-  printf("%s", ourRes);
+    s21_decimal decimal1 = {{0, 0, 0, 1000000000}};
+    s21_decimal decimal2 = {{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0x140000}};
+    s21_decimal result;
+    int code = s21_add(decimal1, decimal2, &result);
 }
